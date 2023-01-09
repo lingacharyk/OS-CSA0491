9eb447a129d498ae9fdf5b618a92332da04f3bcc
lingacharyk <113505257+lingacharyk@users.noreply.github.com>
2023-01-09 11:45:13 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+int main()
+{
+pid_t p;
+printf("before fork\n");
+p=fork();
+if(p==0)
+{
+printf("I am  having student id %d\n",getpid());
+printf("My parent's id is %d\n",getppid());
+}
+else{
+printf("My student's id is %d\n",p);
+printf("I am having parent id %d\n",getpid());
+}
+printf("Common\n");
+}
+
+
